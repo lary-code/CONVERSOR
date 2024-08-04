2790bd3e8b84f29a9925486c2c2f77f036570a1a
lary-code <larissiaferreira3@gmail.com>
2024-08-03 23:55:39 -0300

    Add files via upload

---
+++ CONVERSOR/main.js
@@ -0,0 +1,8 @@
+
+const valorReal = document.querySelector("input")
+
+function converter() {
+    const valorConvertido = valorReal.value / 5
+
+    alert(`O Valor em dolar é ${valorConvertido}`)
+}
